da81bd3a76aacc70ce7d4164e617ba353e23e554
Yihan Yin <han@YihandeMacBook-Pro.local>
2020-12-25 22:32:05 +0800

    feature: complete knapsack examples

--- src/han/Knapsack/Complete/KnapsackComplete.java
+++ src/han/Knapsack/Complete/KnapsackComplete.java
@@ -52,4 +52,131 @@ public class KnapsackComplete implements Knapsack {
         }
         return dp[W];
     }
+
+    /*
+    * leetcode:322
+    * You are given coins of different denominations and a total amount of money amount.
+    * Write a function to compute the fewest number of coins that you need to make up that amount.
+    * If that amount of money cannot be made up by any combination of the coins, return -1.
+    * You may assume that you have an infinite number of each kind of coin.
+    *
+    * Example 1:
+    * Input: coins = [1,2,5], amount = 11
+    * Output: 3
+    * Explanation: 11 = 5 + 5 + 1
+    * */
+    public int coinChange(int[] coins, int amount) {
+        int len=coins.length;
+        // two-dimen
+        /*int[][] dp=new int[len+1][amount+1];
+        for(int i=0;i<=amount;i++) {
+            dp[0][i]=amount+1;
+        }
+        dp[0][0]=0;
+        for(int i=1;i<=len;i++) {
+            int weight=coins[i-1];
+            for(int j=0;j<=amount;j++) {
+                if(j>=weight) {
+                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-weight]+1);
+                }else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[len][amount]==amount+1?-1:dp[len][amount];*/
+        // one-dimen
+        int[] dp=new int[amount+1];
+        for(int i=1;i<=amount;i++) {
+            dp[i]=amount+1;
+        }
+        for(int i=1;i<=len;i++) {
+            int weight=coins[i-1];
+            for(int j=weight;j<=amount;j++) {
+                dp[j]=Math.min(dp[j],dp[j-weight]+1);
+            }
+        }
+        return dp[amount]==amount+1?-1:dp[amount];
+    }
+
+    /*
+    * leetcode:518
+    * You are given coins of different denominations and a total amount of money.
+    * Write a function to compute the number of combinations that make up that amount.
+    * You may assume that you have infinite number of each kind of coin.
+    *
+    * Example 1:
+    * Input: amount = 5, coins = [1, 2, 5]
+    * Output: 4
+    * Explanation: there are four ways to make up the amount:
+    * 5=5
+    * 5=2+2+1
+    * 5=2+1+1+1
+    * 5=1+1+1+1+1
+    * */
+    public int change(int amount, int[] coins) {
+        int len=coins.length;
+        // two-dimen
+        /*int[][] dp=new int[len+1][amount+1];
+        dp[0][0]=1;
+        for(int i=1;i<=len;i++) {
+            int weight=coins[i-1];
+            for(int j=0;j<=amount;j++) {
+                if(j>=weight) {
+                    dp[i][j]=dp[i-1][j]+dp[i][j-weight];
+                }else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[len][amount];*/
+        // one-dimen
+        int[] dp=new int[amount+1];
+        dp[0]=1;
+        for(int i=1;i<=len;i++) {
+            int weight=coins[i-1];
+            for(int j=weight;j<=amount;j++) {
+                dp[j]+=dp[j-weight];
+            }
+        }
+        return dp[amount];
+    }
+
+    /**
+     * special one
+     * leetcode:377
+     * Given an integer array with all positive numbers and no duplicates,
+     * find the number of possible combinations that add up to a positive integer target.
+     *
+     * Example:
+     * nums = [1, 2, 3]
+     * target = 4
+     * The possible combination ways are:
+     * (1, 1, 1, 1)
+     * (1, 1, 2)
+     * (1, 2, 1)
+     * (1, 3)
+     * (2, 1, 1)
+     * (2, 2)
+     * (3, 1)
+     * Note that different sequences are counted as different combinations.
+     * Therefore the output is 7.
+     *
+     * Solution:
+     * different from complete knapsack problem, which examines combination
+     * the core of this problem is permutation
+     * dp[i-num] represents number of sets which end up with num
+     * so dp[i]=Sum(dp[i-num1],dp[i-num2],...)
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp=new int[target+1];
+        dp[0]=1;
+        for(int i=1;i<=target;i++) {
+            for(int num:nums) {
+                if(num<=i) {
+                    dp[i]+=dp[i-num];
+                }
+            }
+        }
+        return dp[target];
+    }
 }
